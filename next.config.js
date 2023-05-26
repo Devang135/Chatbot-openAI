371565b1a8b06cc04189f0d30dc147a9a64b4213
devang <devangnigam22222@gmail.com>
2023-05-26 22:31:12 +0530

    openAIwithFrontEnd

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = {
+  env: {
+    API: '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',
+  },
+};
